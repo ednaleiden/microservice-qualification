2deacefdd9c99a910c8c93d50da21ceb442bbee2
ednaleiden <ednadupont@hotmail.com>
2025-01-16 23:02:49 -0500

    services calificacion

---
+++ src/main/java/com/calificacion/services/CalificacionServicesApplication.java
@@ -0,0 +1,13 @@
+package com.calificacion.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalificacionServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalificacionServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/calificacion/services/CalificacionServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calificacion.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalificacionServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
